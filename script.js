75e0cda9c4fc3b27c09169d8f0c71a0399109b5d
Zoe Smith <107585629+ZoeGrey86@users.noreply.github.com>
2022-07-10 23:46:11 -0500

    Nearly there, But still a fatal flaw
    errors: This page is in Quirks Mode. Page layout may be impacted. For Standards Mode use “<!DOCTYPE html>”.
    Uncaught ReferenceError: require is not defined
        <anonymous> file: scriptHelper.js:2
    solutions found suggest changes to the index.html file.

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 // Write your JavaScript code here!
 
-const { formSubmission } = require("./scriptHelper");
+const scriptHelper = require("./scriptHelper");
 
 window.addEventListener("load", function() {
+    let launchForm = document.getElementById("launchForm");
+    launchForm.addEventListener("submit", function(event) {
+        let list = document.getElementById("faultyItems");
+        let pilot =  document.querySelector("input[name=pilotName]").value;
+        let copilot = document.querySelector("input[name=copilotName]").value;
+        let fuelLevel =  document.querySelector("input[name=fuelLevel]").value;
+        let cargoLevel =  document.querySelector("input[name=cargoMass]").value;
+        scriptHelper.formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
 
-    let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
-    listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-        addDestinationInfo(pickPlanet(listedPlanets));
-    })
-    let submit = document.getElementById("formSubmit")
-    submit.addEventListener("submit", formSubmission)
- });
+        let listedPlanets;
+        // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+        let listedPlanetsResponse = scriptHelper.myFetch();
+        listedPlanetsResponse.then(function (result) {
+            listedPlanets = result;
+            console.log(listedPlanets);
+        }).then(function () {
+            console.log(listedPlanets);
+            // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+            let planet = scriptHelper.pickPlanet(listedPlanets);
+            let document = document.getElementById("missionTarget")
+            let name = planet.name;
+            let diameter = planet.diameter;
+            let star = planet.star;
+            let distance = planet.distance;
+            let moons = planet.moons;
+            let imageUrl = planet.image;
+            scriptHelper.addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl);
+        })
+        event.preventDefault();
+    });
+});
--- scriptHelper.js
+++ scriptHelper.js
@@ -35,59 +35,58 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
      let copilotStatus = document.getElementById('copilotStatus');
      let fuelStatus = document.getElementById('fuelStatus');
      let cargoStatus = document.getElementById('cargoStatus');
-     let Fault = document.getElementById("faultyItems");
-     let launchStatus =document.getElementById("launchStatus");
+     let launchStatus = document.getElementById("launchStatus");
      let launchStatusCheck = document.getElementById("launchStatusCheck");
+
      let testpilot = validateInput(pilot);
      let testcopilot = validateInput(copilot);
      let testfuel = validateInput(fuelLevel);
      let testcargo = validateInput(cargoLevel);
+
       if(testpilot === "empty"){
           pilotStatus.innerHTML = "not ready";
-          Fault.style.visibility = visible;
-          launchStatus.innerHTML = "Shuttle not ready for launch"
-          launchStatusCheck.style.backgroundColor = 'red';
+          list.style.visibility = visible;
+          list.innerHTML += "Shuttle not ready for launch";
+          launchStatusCheck.style.backgroundColor = "rgb(199, 37, 78)";
      }else if(testcopilot==="empty"){
           copilotStatus.innerHTML = "not ready";
-          Fault.style.visibility = visible;
-          launchStatus.innerHTML = "Shuttle not ready for launch"
-          launchStatusCheck.style.backgroundColor = 'red';
+          list.style.visibility = visible;
+          launchStatus.innerHTML = "Shuttle not ready for launch";
+          launchStatusCheck.style.color = "rgb(199, 37, 78)";
      }else if(testfuel === "not a number" || fuelLevel < 10000){
           fuelStatus.innerHTML = "not ready";
-          Fault.style.visibility = visible;
-          launchStatus.innerHTML = "Shuttle not ready for launch"
-          launchStatusCheck.color = "red";
+          list.style.visibility = visible;
+          launchStatus.innerHTML = "Shuttle not ready for launch";
+          launchStatusCheck.color = "rgb(199, 37, 78)";
      }else if(testcargo === "not a number" || cargoLevel > 10000){
           cargoStatus.innerHTML = "not ready";
-          Fault.style.visibility = visible;
+          list.style.visibility = visible;
           launchStatus.innerHTML = "Shuttle not ready for launch";
-          launchStatusCheck.style.color = "red";
+          launchStatusCheck.style.color = "rgb(199, 37, 78)";
      }else{launchStatus.innerHTML = "Shuttle ready for launch";
-          launchStatusCheck.style.color = "green";
+          launchStatusCheck.style.color ="rgb(65, 159, 106)";
           pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
           copilotStatus.innerHTML = `Co-Pilot ${copilot} is ready for launch`;
           fuelStatus.innerHTML = `Fuel level is enough for launch`;
           cargoStatus.innerHTML = `Cargo mass low enough for launch`;
-        } 
-}
+        };
+};
 
 async function myFetch() {
     let planetsReturned;
 
     planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
          response.json().then(function(){
-                console.log(planetsReturned)
           }) 
         });
-
     return planetsReturned;
-}
+};
 
 function pickPlanet(planets) {
-    let num = Math.round(Math.random()*6);
+    let num = Math.round(Math.random()*5);
     let pickedPlanet=planets[num];
     return pickedPlanet
-}
+};
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
